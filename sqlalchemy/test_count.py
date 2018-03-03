9eedb5bbf932b81abba49c9d90dcec919a6cb010
keteng <1193610322@qq.com>
2018-03-04 00:39:01 +0800

    增加sqlalchemy的count计算多少行的测试

---
+++ sqlalchemy/test_count.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+import logging
+
+from contextlib import contextmanager
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine
+from sqlalchemy import func
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+db = 'mysql://boss:boss@127.0.0.1:3306/test?charset=utf8&use_unicode=1'
+
+
+Base = automap_base()
+engine = create_engine(db, echo=False)
+Base.prepare(engine, reflect=True)
+
+factory = sessionmaker(bind=engine)
+Session = scoped_session(factory)
+
+
+@contextmanager
+def session_scope():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+person = Base.classes.Person
+
+
+def add_persons():
+    with session_scope() as session:
+        for i in xrange(10000):
+            body = {
+                "age": i,
+                "firstname": "auto" + str(i)
+            }
+            session.add(person(**body))
+
+
+def count():
+    with session_scope() as session:
+        session.query(func.count(person.id)).scalar()
+
+
+def count_1():
+    with session_scope() as session:
+        session.query(person).count()
+
+
+def count_2():
+    with session_scope() as session:
+        session.query(person).statement.with_only_columns([func.count()]).scalar()
+
+
+if __name__ == '__main__':
+    count_2()
